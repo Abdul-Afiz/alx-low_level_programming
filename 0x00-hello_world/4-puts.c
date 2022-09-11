47de4224e89ad60249e582fc1a3ac7006b4a5b14
Abdul-Afeez <jimohafeez738@gmail.com>
2022-09-11 01:23:30 -0700

    printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main
- *                
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
+ * Return: Always 0.
  */
 int main(void)
 {

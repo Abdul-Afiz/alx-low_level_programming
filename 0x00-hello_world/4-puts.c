0e818710dcfe68039bfb5274ebfdc9aec81d6f62
Abdul-Afeez <jimohafeez738@gmail.com>
2022-09-11 00:46:07 -0700

    printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/** 
+ *  *main - Entry point
+ *   *
+ *    * ReturnValue: zero Always
+ *     */
+int main(void)
+{
+		puts("Programming is like building a multilingual puzzle");
+			return (0);
+}
